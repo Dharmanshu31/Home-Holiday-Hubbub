ae9dbb39ad8a3db812d73bae1fec31866ded576b
Dharmanshu <d.j.paghadal@gmail.com>
2024-05-28 18:37:26 +0530

    adding validation to booking schema

--- src/booking/schemas/booking.schema.ts
+++ src/booking/schemas/booking.schema.ts
@@ -3,28 +3,36 @@ import mongoose, { Document } from 'mongoose';
 
 @Schema()
 export class Booking extends Document {
-  @Prop({ type: mongoose.Schema.ObjectId, ref: 'Property' })
+  @Prop({
+    type: mongoose.Schema.ObjectId,
+    ref: 'Property',
+    required: [true, 'Must Have Property Id'],
+  })
   propertyId: string;
 
-  @Prop({ type: mongoose.Schema.ObjectId, ref: 'User' })
+  @Prop({
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Must Have user Id'],
+  })
   userId: string;
 
-  @Prop({ type: mongoose.Schema.ObjectId })
+  @Prop({ type: mongoose.Schema.ObjectId, required: [true, 'Must Have owner Id'] })
   ownerId: string;
 
   @Prop()
   paymentIntentId: string;
 
-  @Prop()
+  @Prop({ required: [true, 'Must Start Date'] })
   startDate: Date;
 
-  @Prop()
+  @Prop({ required: [true, 'Must End Date'] })
   endDate: Date;
 
-  @Prop()
+  @Prop({ required: [true, 'Must Total Price'] })
   totalPrice: number;
 
-  @Prop()
+  @Prop({ required: [true, 'Must Status'] })
   status: string;
 
   @Prop({ default: Date.now() })
